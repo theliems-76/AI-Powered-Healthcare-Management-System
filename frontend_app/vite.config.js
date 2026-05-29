e7d18b1cc1c3f3a2cc09131cc8b5948cbe44518b
theliems-76 <thienmapvo@gmail.com>
2026-05-29 23:20:53 +0700

    Optimize Vite Build with Code Splitting

--- frontend_app/vite.config.js
+++ frontend_app/vite.config.js
@@ -1,6 +1,30 @@
-﻿import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
+              return 'vendor-react';
+            }
+            if (id.includes('lucide-react')) {
+              return 'vendor-icons';
+            }
+            if (id.includes('axios')) {
+              return 'vendor-network';
+            }
+            if (id.includes('chart.js') || id.includes('react-chartjs-2')) {
+              return 'vendor-charts';
+            }
+            return 'vendor'; // Các thư viện còn lại
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 600,
+  }
 })
